2c3578523b01f741f8b84fe5015f1319db5fdfba
Adam Stankiewicz <sheerun@sher.pl>
2019-03-12 23:15:36 +0100

    Fix paths

--- example/migrate.js
+++ example/migrate.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-const knexMigrate = require('../package/src')
+const knexMigrate = require('../src')
 
 // It has following signature:
 // knexMigrate(command: String, flags: Object, progress: Function)
